56cb58ef3320c9b85233bd8e29573e709869a296
BERNARD BAY <superway1112@gmail.com>
2023-03-16 20:26:12 -0700

    com 3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
-#include<stdio.>
+#include<stdio.h>
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
 int main(void){
-	printf("Size of a char: %d byte(s)\n",sizeof(float));return 0;    
+	printf("Size of a char: %d byte(s)\n",sizeof(char)) 
+	printf("Size of an int: %d byte(s)\n",sizeof(int));
+	printf("Size of an long int: %d byte(s)\n",sizeof(long int));
+	printf("Size of an long long int: %d byte(s)\n",sizeof(long long int));	
+	printf("Size of an float: %d byte(s)\n",sizeof(float));
+	return (0);
 } 
