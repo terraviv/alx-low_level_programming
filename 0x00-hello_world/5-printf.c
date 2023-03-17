ba7f54c4e72131353f45d003dcdd6be03facf0d1
BERNARD BAY <superway1112@gmail.com>
2023-03-16 19:55:20 -0700

    com 1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,6 @@
+#include<stdio.>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main(void){
+	printf("Size of a char: %d byte(s)\n",sizeof(float));return 0;    
+} 
