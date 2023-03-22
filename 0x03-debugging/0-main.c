5e1227b71e38e83776c7f0f588a3e4cf8ec9e18a
BERNARD BAY <superway1112@gmail.com>
2023-03-21 22:54:05 -0700

    debug 101

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+ main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+
 
 int main(void)
 {
@@ -14,4 +15,3 @@ int main(void)
 
 	return (0);
 }
-
