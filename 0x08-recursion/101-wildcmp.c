7bb3446cbb98acd0dc7bb8e956c61121cb756489
BERNARD BAY <superway1112@gmail.com>
2023-04-05 10:57:49 -0700

    0x08. C - Recursion test 14

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * wildcmp - function for comparison of strings.
+ * @s1: param 1.
+ * @s2: param 2.
+ * Return: an integer is return.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+
+if (*s2 == '*')
+{
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)); }
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1)); }
+else if (*s1 == '\0')
+{
+if (*s2 == '\0' || *s2 != '*')
+{
+return (*s2 == '\0'); }
+else
+{
+return (wildcmp(s1, s2 + 1)); }
+}
+else
+{
+return (0); }}
+
