7e0b411906de97ad30334d247dbbe6eed3256915
BERNARD BAY <superway1112@gmail.com>
2023-04-05 11:05:07 -0700

    0x08. C - Recursion test 16

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,14 +8,7 @@
 
 int wildcmp(char *s1, char *s2)
 {
-
-if (*s2 == '*')
-{
-return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)); }
-else if (*s1 == *s2)
-{
-return (wildcmp(s1 + 1, s2 + 1)); }
-else if (*s1 == '\0')
+if (*s1 == '\0')
 {
 if (*s2 == '*' && *s2 != '\0')
 {
@@ -24,6 +17,12 @@ else
 {
 return (wildcmp(s1, s2 + 1)); }
 }
+if (*s2 == '*')
+{
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)); }
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1)); }
 else
 {
 return (0); }}
