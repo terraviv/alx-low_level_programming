c80e93c6e2c39ecbd9e18cb11fd4498c6fdb1248
BERNARD BAY <superway1112@gmail.com>
2023-04-07 10:15:45 -0700

    0x0A. C - argc, argv test 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include "main.h"
 /**
  * main - shows whatsmyname
@@ -7,9 +8,10 @@
  * @argv: param 2
  * Return: an integer is return.
  */
-void main(int argc __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	write(STDOUT_FILENO, argv[0], strlen(argv[0]));
 	write(STDOUT_FILENO, "\n", 1);
+	return (0);
 }
 
