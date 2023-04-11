9a28e768a5a26a8674b45a45ea75676fc530b3c0
BERNARD BAY <superway1112@gmail.com>
2023-04-11 14:23:18 -0700

    0x0B. C - malloc, free test 6

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,11 +8,13 @@
  */
 char *_strdup(char *str)
 {
+	size_t my_char_length;
+	char *my_new_string;
+
 	if (str == NULL)
 		return (NULL);
-	size_t my_char_length = strlen(str);
-	char *my_new_string = malloc(sizeof(char) * (my_char_length + 1));
-
+	my_char_length = strlen(str);
+	my_new_string = malloc(sizeof(char) * (my_char_length + 1));
 	if (my_new_string == NULL)
 		return (NULL);
 	strcpy(my_new_string, str);
