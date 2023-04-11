f9b8ccf586bfe25c2123914ffca665e9587cce22
BERNARD BAY <superway1112@gmail.com>
2023-04-11 15:42:15 -0700

    0x0B. C - malloc, free test 10

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - function that concatenates all the arguments.
+ * @ac: param 1.
+ * @av: param 2.
+ * Return: a char is return.
+ */
+char *argstostr(int ac, char **av)
+{
+	int x1, x2, x3 = 0, my_len;
+	char *my_output;
+
+	if (av == NULL || ac == 0)
+		return (NULL);
+	my_len = 0;
+	for (x1 = 0; x1 < ac; x1++)
+	{
+		for (x2 = 0; av[x1][x2]; x2++)
+			my_len++;
+		my_len++;
+	}
+	my_output = malloc(sizeof(char) * my_len);
+	if (my_output == NULL)
+		return (NULL);
+	for (x1 = 0; x1 < ac; x1++)
+	{
+	for (x2 = 0; av[x1][x2]; x2++)
+	{
+		my_output[x3] = av[x1][x2];
+		x3++;
+	}
+	my_output[x3] = '\n';
+	x3++;
+	}
+	my_output[x3] = '\0';
+	return (my_output);
+}
+
