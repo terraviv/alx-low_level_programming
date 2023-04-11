1e76aec283e2f98591e9958f34f03fb00056ada3
BERNARD BAY <superway1112@gmail.com>
2023-04-11 16:39:35 -0700

    0x0B. C - malloc, free test 15

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,8 +13,8 @@ char **strtow(char *str)
 	int x1, x2, x3, my_word_cp = 0;
 	int my_word_len;
 	char **my_words;
-
-	if (*str == '\0' || str == NULL)
+	
+	if (*str == '\0' || str == NULL || str == " ")
 		return (NULL);
 	my_word_len = strlen(str);
 	for (x1 = 0; x1 < my_word_len; x1++)
@@ -22,7 +22,7 @@ char **strtow(char *str)
 		if ((x1 == 0 || isspace(str[x1 - 1])) && !isspace(str[x1]))
 			my_word_cp = my_word_cp + 1;
 	}
-	my_words = (char **) malloc(sizeof(char *) * (my_word_cp));
+	my_words = (char **) malloc(sizeof(char *) * (my_word_cp + 1));
 	if (my_words == NULL)
 		return (NULL);
 	for (x1 = 0, x2 = 0; x2 < my_word_cp; x2++)
@@ -32,7 +32,7 @@ char **strtow(char *str)
 		x3 = x1;
 		while (x3 < my_word_len && !isspace(str[x3]))
 			x3++;
-		my_words[x2] = (char *) malloc(sizeof(char *) * (x3 - x1));
+		my_words[x2] = (char *) malloc(sizeof(char *) * (x3 - x1 - 1));
 		if (my_words[x2] == NULL)
 		{
 			for (x1 = 0; x1 < x2; x1++)
