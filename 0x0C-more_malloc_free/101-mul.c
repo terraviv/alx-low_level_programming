8abebe6c6883dcf606892c73b540f3b216d00153
BERNARD BAY <superway1112@gmail.com>
2023-04-13 18:52:17 -0700

    0x0C. C - More malloc, free test 9

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,44 +11,53 @@
  */
 int main(int argc, char *argv[])
 {
-int myc1, myc2, leni2;
-int leni1, *moutv;
-char *inp1 = argv[1], *inp2 = argv[2];
-if (argc != 3)
-{
-printf("Error\n");
-exit(98); }
-leni1 = strlen(inp2);
-leni2 = strlen(inp1);
-for (myc1 = 0; myc1 < leni2; myc1++)
-{
-if (!isdigit(inp1[myc1]))
-{
-printf("Error\n");
-exit(98); } }
-for (myc1 = 0; myc1 < leni1; myc1++)
-{
-if (!isdigit(inp2[myc1]))
-{
-printf("Error\n");
-exit(98); } }
-moutv = malloc(sizeof(int) * (leni1 + leni2 + 1));
-if (!moutv)
-return (1);
-for (myc1 = leni2 - 1; myc1 >= 0; myc1--)
-{
-int ca = 0;
-for (myc2 = leni1 - 1; myc2 >= 0; myc2--)
-{
-int gi = (inp2[myc2] - '0') * (inp1[myc1] - '0') + ca + moutv[1 + myc1 + myc2];
-ca = gi / 10;
-moutv[1 + myc1 + myc2] = gi % 10; }
-moutv[myc1] = moutv[myc1] + ca; }
-myc1 = 0;
-while ((myc1 < (leni1 + leni2))&&moutv[myc1] == 0)
-myc1 = 1 + myc1;
-for (; myc1 < leni1 + leni2; myc1++)
-printf("%d", moutv[myc1]);
-printf("\n");
-return (0); }
+	int mycounter1, mycounter2, length_of_my_input_first_number, length_of_my_input_second_number, *my_output_var;
+	char *my_input_first_number = argv[1];
+	char *my_input_second_number = argv[2];
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	length_of_my_input_second_number = strlen(my_input_second_number);
+	length_of_my_input_first_number = strlen(my_input_first_number);
+	for (mycounter1 = 0; mycounter1 < length_of_my_input_first_number; mycounter1++)
+	{
+		if (!isdigit(my_input_first_number[mycounter1]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	for (mycounter1 = 0; mycounter1 < length_of_my_input_second_number; mycounter1++)
+	{
+		if (!isdigit(my_input_second_number[mycounter1]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	my_output_var = malloc(sizeof(int) * (length_of_my_input_second_number + length_of_my_input_first_number + 1));
+	if(!my_output_var)
+		return (1);
+	for (mycounter1 = length_of_my_input_first_number -1; mycounter1 >= 0; mycounter1--)
+	{
+		int my_carry_var = 0;
+		for (mycounter2 = length_of_my_input_second_number - 1; mycounter2>=0; mycounter2--)
+		{
+			int my_digit_variable = (my_input_second_number[mycounter2] - '0') * (my_input_first_number[mycounter1] - '0') + my_carry_var + my_output_var[1+mycounter1+mycounter2];
+			my_carry_var = my_digit_variable / 10;
+			my_output_var[1+mycounter1+mycounter2] = my_digit_variable % 10;
+		}
+		my_output_var[mycounter1] = my_output_var[mycounter1] + my_carry_var;
+	}
+	mycounter1 = 0;
+	while (mycounter1 < (length_of_my_input_second_number + length_of_my_input_first_number) && my_output_var[mycounter1] == 0)
+		mycounter1 = 1 + mycounter1;
+	for (; mycounter1 < length_of_my_input_second_number + length_of_my_input_first_number; mycounter1++)
+		printf("%d", my_output_var[mycounter1]);
+	printf("\n");
+	free(my_output_var);
+	return (0);
+}
 
