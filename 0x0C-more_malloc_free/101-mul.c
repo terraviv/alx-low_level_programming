356714a28ce77a8675b477b669cf8aead0f75a47
BERNARD BAY <superway1112@gmail.com>
2023-04-13 18:44:56 -0700

    0x0C. C - More malloc, free test 8

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,39 +1,54 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
  * main - function name
  * @argc: param 1.
  * @argv: param 2.
- * Return: always 0 (Success)
+ * Return: an integer is return.
  */
 int main(int argc, char *argv[])
 {
-	int mycounter1, my_first_number, my_second_number;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (mycounter1 = 1; mycounter1 < argc; mycounter1++)
-	{
-		char *a_pointer_1 = argv[mycounter1];
-
-		while (*a_pointer_1 != '\0')
-		{
-			if (!isdigit(*a_pointer_1))
-			{
-				printf("Error\n");
-				exit(98);
-			}
-			a_pointer_1 = a_pointer_1 + 1;
-		}
-	}
-	my_first_number = atoi(argv[1]);
-	my_second_number = atoi(argv[2]);
-	printf("%d\n", my_first_number * my_second_number);
-	exit(0);
-}
+int myc1, myc2, leni2;
+int leni1, *moutv;
+char *inp1 = argv[1], *inp2 = argv[2];
+if (argc != 3)
+{
+printf("Error\n");
+exit(98); }
+leni1 = strlen(inp2);
+leni2 = strlen(inp1);
+for (myc1 = 0; myc1 < leni2; myc1++)
+{
+if (!isdigit(inp1[myc1]))
+{
+printf("Error\n");
+exit(98); } }
+for (myc1 = 0; myc1 < leni1; myc1++)
+{
+if (!isdigit(inp2[myc1]))
+{
+printf("Error\n");
+exit(98); } }
+moutv = malloc(sizeof(int) * (leni1 + leni2 + 1));
+if (!moutv)
+return (1);
+for (myc1 = leni2 - 1; myc1 >= 0; myc1--)
+{
+int ca = 0;
+for (myc2 = leni1 - 1; myc2 >= 0; myc2--)
+{
+int gi = (inp2[myc2] - '0') * (inp1[myc1] - '0') + ca + moutv[1 + myc1 + myc2];
+ca = gi / 10;
+moutv[1 + myc1 + myc2] = gi % 10; }
+moutv[myc1] = moutv[myc1] + ca; }
+myc1 = 0;
+while ((myc1 < (leni1 + leni2))&&moutv[myc1] == 0)
+myc1 = 1 + myc1;
+for (; myc1 < leni1 + leni2; myc1++)
+printf("%d", moutv[myc1]);
+printf("\n");
+return (0); }
 
