eeaaa9cdd5824ab92611dda6bfe524b73db0fb79
BERNARD BAY <superway1112@gmail.com>
2023-04-17 17:06:30 -0700

    0x0E. C - Structures, typedef test 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: element
+ * @age: element
+ * @owner: element
+ */
+struct dog
+{
+	char *owner;
+	char *name;
+	float age; };
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void show_the_dog(struct dog *infos);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
+
