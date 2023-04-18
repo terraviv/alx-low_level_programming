1b6a4a6adf0ca91300f6aa3aa445504c841bba9f
BERNARD BAY <superway1112@gmail.com>
2023-04-17 17:52:47 -0700

    0x0E. C - Structures, typedef test 7

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *infos)
 	if (infos == NULL)
 		return;
 	printf("Name: %s\n", (infos->name != NULL) ? infos->name : "(nil)");
-	printf("Age: %.f\n", infos->age);
+	printf("Age: %f\n", infos->age);
 	printf("Owner: %s\n", (infos->owner != NULL) ? infos->owner : "(nil)");
 }
 
