22ec8a638421698c30ffb87a3d7dbf823611e9b0
BERNARD BAY <superway1112@gmail.com>
2023-04-27 08:23:00 -0700

    0x12. C - Singly linked lists test 9

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+void no_way_fyun(void) __attribute__ ((constructor));
+/**
+ * no_way_fyun - f1
+ * Return: po
+ */
+void no_way_fyun(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
