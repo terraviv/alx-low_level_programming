5519db943eca7d4f8c2e726db250cad74eb3efb7
BERNARD BAY <superway1112@gmail.com>
2023-04-27 07:11:23 -0700

    0x12. C - Singly linked lists test 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,17 +8,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *var1;
-unsigned int yum;
-yum = 0;
+unsigned int len;
 
-while (str[yum])
-yum++;
-
+len = 0;
+while (str[len])
+len = 1 + len;
 var1 = malloc(sizeof(list_t));
 if (!var1)
 return (NULL);
 var1->str = strdup(str);
-var1->yum = yum;
+var1->len = len;
 var1->next = (*head);
 (*head) = var1;
 return (*head);
