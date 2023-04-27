3ba32c09e8d84a9071ee58e80ae60cbf51fea78b
BERNARD BAY <superway1112@gmail.com>
2023-04-27 08:16:38 -0700

    0x12. C - Singly linked lists test 7

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - pj
+ * @head: pj
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *pos;
+	while (head != NULL)
+	{
+		pos = head->next;
+		free(head->str);
+		free(head);
+		head = pos;
+	}
+}
+
