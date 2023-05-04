db1c037f47ef48dd5b8cb82ee0b06a96f0ca35c4
BERNARD BAY <superway1112@gmail.com>
2023-05-04 12:08:43 -0700

    0x14. C - Bit manipulation test 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - azzq4
+ * Return: ttiti
+ */
+int get_endianness(void)
+{
+unsigned int mapdpdpdlfpefl = 1;
+char *c = (char *) &mapdpdpdlfpefl;
+return ((int)*c);
+}
+
