ff57310e8b1364d45cee06c1e5a5e0688f56b5c2
BERNARD BAY <superway1112@gmail.com>
2023-05-08 16:58:01 -0700

    0x15. C - File I/O test 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,22 +16,22 @@ uyt_filedesc = open(filename, O_RDONLY);
 if (uyt_filedesc == -1 || filename == NULL || my_buf45 == NULL)
 {
 free(my_buf45);
-close(fd);
+close(uyt_filedesc);
 return (0);
 }
 k1 = read(uyt_filedesc, my_buf45, letters);
 if (k1 == -1)
 {
 free(my_buf45);
-close(fd);
+close(uyt_filedesc);
 return (0);
 }
-my_buf45[n] = '\0';
+my_buf45[k1] = '\0';
 k2 = write(STDOUT_FILENO, my_buf45, k1);
 if (k2 != k1 || k2 == -1)
 {
 free(my_buf45);
-close(fd);
+close(uyt_filedesc);
 return (0);
 }
 free(my_buf45);
