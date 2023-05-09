58450f5795c516feab8b6ee7099e705af373721a
BERNARD BAY <superway1112@gmail.com>
2023-05-08 19:48:22 -0700

    0x15. C - File I/O test 9

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <elf.h>
 #define BU40_7 1024 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
